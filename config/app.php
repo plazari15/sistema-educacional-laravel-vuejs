166e171e2474e925f4b259eb43f79e5955260b76
Pedro <plazari96@gmai.com>
2017-07-16 20:19:43 -0300

    Configurando menu em PHP]

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'SONEdu'),
 
     /*
     |--------------------------------------------------------------------------
@@ -230,6 +230,8 @@ return [
         'Table' => 'Bootstrapper\Facades\Table',
         'Button' => 'Bootstrapper\Facades\Button',
 	    "Icon"   => \Bootstrapper\Facades\Icon::class,
+	    "Navbar" => Bootstrapper\Facades\Navbar::class,
+	    "Navigation" => Bootstrapper\Facades\Navigation::class,
 
     ],
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,61 +15,36 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top navbar-inverse">
-            <div class="container">
-                <div class="navbar-header">
+        @php
+            $navBar = Navbar::withBrand(config('app.name'), route('admin.dashboard'))->inverse();
+            if(Auth::check()){
+                $arrLinks = [
+                    ['link' => route('admin.users.index'), 'title' => 'Usuário'],
+                ];
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                $arrLinksRight = [
+                    [
+                        Auth::user()->name,
+                        [
+                            ['link' => route('logout'), 'title' => 'Logout', 'linkAttributes' => ['onclick' => "event.preventDefault();
+                                                     document.getElementById(\"logout-form\").submit();"]]
+                        ]
+                    ]
+                ];
+                $navBar->withContent(Navigation::links($arrLinks))
+                       ->withContent(Navigation::links($arrLinksRight)->right());
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+                $logoutForm = FormBuilder::plain([
+                                'id' => 'logout-form',
+                                'url' => route('logout'),
+                                'method' => 'POST',
+                                'style' => 'display:none'
+                                ]);
+            form($logoutForm);
+            }
+        @endphp
+        {!! $navBar !!}
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         @yield('content')
     </div>
